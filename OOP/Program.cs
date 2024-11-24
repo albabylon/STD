799ca0c778f70182e8163d4eae90adfbbc2bd835
aveA <aveA@AVE>
2024-11-24 12:07:31 +0300

    сравнение класс и стркткры

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -8,14 +8,41 @@ namespace OOP
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
+            ObjClass obj1 = new ObjClass() { Str = "Value1", Intgr = 1, Rectangle = new Rectangle() { a = 1, b = 1 } };
+            ObjClass obj2 = obj1;
+
+            obj1.Str = "Value2";
+            obj1.Intgr = 2;
+            obj1.Rectangle = new Rectangle() { a = 2, b = 2 };
+
+            ObjStruct objStr1 = new ObjStruct() { Str = "Value1", Intgr = 1, Rectangle = new Rectangle() { a = 1, b = 1 } };
+            ObjStruct objStr2 = objStr1;
+
+            objStr1.Str = "Value2";
+            objStr1.Intgr = 2;
+            objStr1.Rectangle = new Rectangle() { a = 2, b = 2 };
+        }
+
+        class ObjClass
+        {
+            public string Str;
+            public int Intgr;
+            public Rectangle Rectangle;
+        }
+        struct ObjStruct
+        {
+            public string Str;
+            public int Intgr;
+            public Rectangle Rectangle;
         }
 
         class Rectangle
         {
-            double a;
-            double b;
+            public double a;
+            public double b;
 
             public Rectangle()
             {
