96ef88390909ea2f381dc3855b0bc3beee4375e3
babylone <barsukovvo@mail.ru>
2024-12-02 22:07:04 +0300

    generic

---
+++ OOP/Generic.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class Generic
+    {
+        public abstract class Car<TEngine> where TEngine : Engine
+        {
+            public TEngine Engine = default(TEngine);
+            public virtual void ChangePart<TPart>(TPart newPart) where TPart : Part
+            {
+
+            }
+        }
+        public class ElectricalCar : Car<ElectricEngine>
+        {
+            public override void ChangePart<TPart>(TPart newPart)
+            {
+
+            }
+        }
+        public class GasCar : Car<GasEngine>
+        {
+            public override void ChangePart<TPart>(TPart newPart)
+            {
+
+            }
+        }
+
+        public abstract class Engine
+        {
+
+        }
+        public class ElectricEngine : Engine
+        {
+
+        }
+        public class GasEngine : Engine
+        {
+
+        }
+
+        public abstract class Part 
+        {
+            
+        }
+        public class Battery : Part
+        {
+
+        }
+        public class Differential : Part
+        {
+
+        }
+        public class Wheel : Part
+        {
+            
+        }
+    }
+}
--- OOP/Program.cs
+++ OOP/Program.cs
@@ -87,16 +87,19 @@ namespace OOP
             //Console.WriteLine(num1); //58
             //Console.WriteLine(num2); //3
 
-            int num1 = 7;
-            int num2 = -13;
-            int num3 = 0;
+            //int num1 = 7;
+            //int num2 = -13;
+            //int num3 = 0;
 
-            Console.WriteLine(num1.GetNegative()); //-7
-            Console.WriteLine(num1.GetPositive()); //7
-            Console.WriteLine(num2.GetNegative()); //-13
-            Console.WriteLine(num2.GetPositive()); //13
-            Console.WriteLine(num3.GetNegative()); //0
-            Console.WriteLine(num3.GetPositive()); //0
+            //Console.WriteLine(num1.GetNegative()); //-7
+            //Console.WriteLine(num1.GetPositive()); //7
+            //Console.WriteLine(num2.GetNegative()); //-13
+            //Console.WriteLine(num2.GetPositive()); //13
+            //Console.WriteLine(num3.GetNegative()); //0
+            //Console.WriteLine(num3.GetPositive()); //0
+
+
+
 
             Console.ReadKey();
         }
