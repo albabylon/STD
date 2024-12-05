d19c6d335e875896b92e76858bb32184f0648901
babylone <barsukovvo@mail.ru>
2024-12-06 00:44:27 +0300

    files1

--- Files/Program.cs
+++ Files/Program.cs
@@ -12,5 +12,30 @@ namespace Files
         {
 
         }
+
+        public class Drive
+        {
+            public string Name { get; }
+            public long TotalSpace { get; }
+            public long FreeSpace { get; }
+
+            public Drive(string name, long totalSpace, long freeSpace)
+            {
+                Name = name;
+                TotalSpace = totalSpace;
+                FreeSpace = freeSpace;
+            }
+        }
+
+        public class Folder
+        {
+            public List<string> Files { get; set; } = new List<string>();
+        }
+
+        Dictionary<string, Folder> Folders = new Dictionary<string, Folder>();
+        public void CreateNewFolder(string name)
+        {
+            Folders.Add(name, new Folder());
+        }
     }
 }
