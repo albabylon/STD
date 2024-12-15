d0f3f2f1295e77dd0e85399d92245394280fbab0
aveA <aveA@AVE>
2024-12-15 14:29:19 +0300

    исключения

---
+++ ExceptionsDelegateFinalTask/ExeptionsTask1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ExceptionsDelegateEventsFinalTask
+{
+    public class ExeptionsTask1
+    {
+        public class NotUniqElemenToAddException : Exception
+        {
+            public string ElementName { get; set; }
+
+            public NotUniqElemenToAddException()
+            {
+
+            }
+            public NotUniqElemenToAddException(string message) : base(message)
+            {
+                
+            }
+
+            public NotUniqElemenToAddException(string message, string elementName) : this(message)
+            {
+                ElementName = elementName;
+            }
+        }
+    }
+}
---
+++ ExceptionsDelegateFinalTask/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using static ExceptionsDelegateEventsFinalTask.ExeptionsTask1;
+
+namespace ExceptionsDelegateEventsFinalTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Исключения
+            Exception[] exceptions = new Exception[5];
+            exceptions[0] = new IndexOutOfRangeException();
+            exceptions[1] = new FormatException();
+            exceptions[2] = new ArgumentNullException();
+            exceptions[3] = new DivideByZeroException();
+            exceptions[4] = new NotUniqElemenToAddException("Добавлен не уникальный элемент", "Элемент 012");
+
+            for (int i = 0; i < exceptions.Length; i++) 
+            {
+                try
+                {
+                    throw exceptions[i];
+                }
+                catch (IndexOutOfRangeException ex)
+                {
+                    Console.WriteLine($"Ошибка: {ex.Message}");
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine($"Ошибка: {ex.Message}");
+                }
+                catch (ArgumentNullException ex)
+                {
+                    Console.WriteLine($"Ошибка: {ex.Message}");
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine($"Ошибка: {ex.Message}");
+                }
+                catch (NotUniqElemenToAddException ex)
+                {
+                    Console.WriteLine($"Ошибка: {ex.Message}, Элемент: {ex.ElementName}");
+                }
+                finally 
+                {
+                    Console.WriteLine("------------------");
+                }
+            }
+
+            //Делегаты и события
+
+
+
+
+           Console.ReadKey();
+        }
+    }
+}
