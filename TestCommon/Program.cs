2995c448a711fb382dd164b2e8c812656bae738a
babylone <barsukovvo@mail.ru>
2024-12-19 21:50:10 +0300

    test

---
+++ TestCommon/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestCommon
+{
+    internal class Program
+    {
+        public static List<string> Error { get; set; } = new List<string>();
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Method();
+            }
+            catch (Exception ex) 
+            { 
+
+            }
+
+            Console.WriteLine(string.Join(", ", Error));
+
+            Console.ReadLine();
+        }
+
+        private static void Method()
+        {
+            List<string> list = new List<string>() { "sasha", "masha", "dasha" };
+            foreach (string l in list)
+            {
+                try
+                {
+                    throw new Exception(l);
+                }
+                catch (Exception e)
+                {
+                    Error.Add(l);
+                }
+            }
+        }
+    }
+}
