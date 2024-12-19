dc7c184423c25b1620e083d3bc47897135c3659f
babylone <barsukovvo@mail.ru>
2024-12-19 22:43:17 +0300

    интерфейсы

---
+++ InterfacesDIExample/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesDIExample
+{
+    internal class Program
+    {
+        static ILogger logger {get; set;}
+        static void Main(string[] args)
+        {
+            logger = new Logger();
+            var worker1 = new Worker1(logger);
+            var worker2 = new Worker2(logger);
+            var worker3 = new Worker3(logger);
+
+            worker1.Work();
+            worker2.Work();
+            worker3.Work();
+
+            Console.ReadLine();
+        }
+
+    }
+
+    //DI - внедрены зависимости через интерфейсы
+    public interface ILogger
+    {
+        void Event(string message);
+        void Error(string message);
+    }
+    public class Logger : ILogger
+    {
+        public void Error(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void Event(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public interface IWorker
+    {
+        void Work();
+    }
+}
---
+++ InterfacesDIExample/Worker1.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading;
+
+namespace InterfacesDIExample
+{
+    public class Worker1 : IWorker
+    {
+        ILogger logger { get; }
+        public Worker1(ILogger logger)
+        {
+            this.logger = logger;
+        }
+        public void Work()
+        {
+            logger.Event("Worker 1 начал работу");
+            Thread.Sleep(3000);
+            logger.Event("Worker 1 закончил работу");
+        }
+    }
+
+    public class Worker2 : IWorker
+    {
+        ILogger logger { get; }
+        public Worker2(ILogger logger)
+        {
+            this.logger = logger;
+        }
+        public void Work()
+        {
+            logger.Event("Worker 2 начал работу");
+            Thread.Sleep(3000);
+            logger.Event("Worker 2 закончил работу");
+        }
+    }
+
+    public class Worker3 : IWorker
+    {
+        ILogger logger { get; }
+        public Worker3(ILogger logger)
+        {
+            this.logger = logger;
+        }
+        public void Work()
+        {
+            logger.Event("Worker 3 начал работу");
+            Thread.Sleep(3000);
+            logger.Event("Worker 3 закончил работу");
+        }
+    }
+}
