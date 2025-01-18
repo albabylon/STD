0d98ba1438c5e6a87e40f29520d5502a8176e17d
aveA <aveA@AVE>
2025-01-18 15:36:04 +0300

    Задание 13.6.1

---
+++ CollectionFinalTask/ListLinkedListComparer.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace CollectionFinalTask
+{
+    public class ListLinkedListComparer
+    {
+        public List<char> charsByList;
+        public LinkedList<char> charsByLinkedList;
+
+        public ListLinkedListComparer()
+        {
+            charsByList = new List<char>();
+            charsByLinkedList = new LinkedList<char>();
+        }
+
+        public ListLinkedListComparer(string text) : this()
+        {
+            AddingCharToList(text, charsByList);
+            AddingCharToList(text, charsByLinkedList);
+        }
+
+        public void CompareCharInsertingTime(char testChar, string testString)
+        {
+            Console.WriteLine();
+            Console.WriteLine("СРАВНЕНИЕ ВРЕМЕНИ ДОБАВЛЕНИЯ В --LIST-- И --LINKEDLIST-- В СЕРЕДИНУ 1 СИМВОЛА и МАССИВА СИМВОЛОВ (ТЕКСТА)");
+
+            // List
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            charsByList.Insert(charsByList.Count / 2, testChar);
+            string timeCharsByList = stopwatch.Elapsed.TotalMilliseconds.ToString();
+
+            stopwatch.Restart();
+            charsByList.InsertRange(charsByList.Count / 2, testString);
+            string timeStringByList = stopwatch.Elapsed.TotalMilliseconds.ToString();
+
+
+
+            // поиск серединного нода, пропускаем в подсчете времени, так как учитываем только вставку
+            char middleChar = charsByLinkedList.ElementAt(charsByLinkedList.Count / 2);
+            var middleNode = charsByLinkedList.Find(middleChar);
+
+
+
+            // LinkedList
+            stopwatch.Restart();
+            charsByLinkedList.AddAfter(middleNode, testChar);
+            string timeCharsByLinkedList = stopwatch.Elapsed.TotalMilliseconds.ToString();
+
+            stopwatch.Restart();
+            foreach (char c in testString) 
+            {
+                charsByLinkedList.AddAfter(middleNode, c);
+                middleNode = middleNode.Next;
+            }
+            string timeStringByLinkedList = stopwatch.Elapsed.TotalMilliseconds.ToString();
+
+            Console.WriteLine();
+            Console.WriteLine($"Добавление символа:");
+            Console.WriteLine($"Время List: {timeCharsByList} мс");
+            Console.WriteLine($"Время LinkedList: {timeCharsByLinkedList} мс");
+            Console.WriteLine($"List быстрее добавляет в середину 1 элемент, LinkedList чем в {(int)(double.Parse(timeCharsByLinkedList) / double.Parse(timeCharsByList))} раз");
+
+            Console.WriteLine();
+            Console.WriteLine($"Добавление строки:");
+            Console.WriteLine($"Время List: {timeStringByList} мс");
+            Console.WriteLine($"Время LinkedList: {timeStringByLinkedList} мс");
+            Console.WriteLine($"LinkedList быстрее добавляет в середину массив элементов, чем List в {(int)(double.Parse(timeStringByList) / double.Parse(timeStringByLinkedList))} раз");
+        }
+        
+        private void AddingCharToList(string text, List<char> list)
+        {
+            foreach (char ch in text)
+            {
+                list.Add(ch);
+            }
+        }
+        private void AddingCharToList(string text, LinkedList<char> list)
+        {
+            foreach (char ch in text)
+            {
+                list.AddFirst(ch);
+            }
+        }
+    }
+}
---
+++ CollectionFinalTask/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace CollectionFinalTask
+{
+    internal class Program
+    {
+        private const string path = "https://drive.google.com/uc?export=download&id=1jg43arS4KIUwO0-kao_ga7PWIPWBOXxo";
+        private const string fileName = "input.txt";
+
+        static void Main(string[] args)
+        {
+            if (!CheckFileExist())
+            {
+                DowmloadFile();
+            }
+            else
+                Console.WriteLine("Файл уже был скачан");
+            string text = ReadFile() ?? throw new Exception("Пустая переменная с текстом");
+
+            // Задание 13.6.1 - Сравнение времени добавления в середину списка для List и LinkedList
+            ListLinkedListComparer listLinkedListComparer = new ListLinkedListComparer(text);
+            listLinkedListComparer.CompareCharInsertingTime('$', "ЧЕМ ДЛИНЕЕ ТЕКСТ ТЕМ ЭФФЕКТИВНЕЕ РАБОТА LINKEDLIST");
+
+
+            // Задание 13.6.2
+
+            Console.ReadLine();
+        }
+
+        private static void DowmloadFile()
+        {
+            Console.WriteLine("Начато скачивание файла...");
+
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    client.DownloadFile(path, fileName);
+                }
+            }
+            catch (Exception ex) { Console.WriteLine($"Ошибка скачивания:\n{ex}"); }
+
+            Console.WriteLine("Cкачивание файла выполнено!");
+        }
+        private static bool CheckFileExist()
+        {
+            if (File.Exists(fileName))
+                return true;
+            return false;
+        }
+        private static string ReadFile()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            Console.WriteLine("Начато считывание файла...");
+
+            using (StreamReader sr = File.OpenText(fileName))
+            {
+                while (sr.ReadLine() != null)
+                {
+                    sb.AppendLine(sr.ReadLine());
+                }
+            }
+
+            Console.WriteLine("Cчитывание файла выполнено!");
+
+            return sb.ToString();
+        }
+    }
+}
