088f98015a79f1c940e3eb472eca20d392603734
babylone <barsukovvo@mail.ru>
2025-01-22 23:30:37 +0300

    linq

---
+++ LINQ1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Linq;
+using System;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace LINQ1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Разновидность LINQ
+            //LINQ to Objects: применяется для работы с массивами и коллекциями;
+            //LINQ to Entities: применяется для работы с базой данных через Entity Framework;
+            //LINQ to SQL: используется как технология доступа к данным в MS SQL Server;
+            //LINQ to XML: применяется при работе с файлами XML;
+            //LINQ to DataSet: применяется при работе с объектом DataSet;
+            //Parallel LINQ(PLINQ): используется для выполнения параллельных запросов.
+
+
+            // LINQ TO OBJECTS
+
+            string[] people = { "Анна", "Мария", "Сергей", "Алексей", "Дмитрий", "Ян" };
+
+            // LINQ выражение
+            var selectedPeople = from p in people // промежуточная переменная p 
+                                 where p.StartsWith("А") // фильтрация по условию
+                                 orderby p // сортировка по возрастанию (дефолтная)
+                                 select p; // выбираем объект и сохраняем в выборку
+
+            // LINQ расширений
+            selectedPeople = people.Where(p => p.StartsWith("А")).OrderBy(p => p);
+
+            foreach (string s in selectedPeople)
+                Console.WriteLine(s);
+        }
+
+        // Фильтрация
+        // SelectMany()
+        void Method()
+        {
+            // Подготовим данные
+            List<Student> students = new List<Student>
+            {
+               new Student {Name="Андрей", Age=23, Languages = new List<string> {"английский", "немецкий" }},
+               new Student {Name="Сергей", Age=27, Languages = new List<string> {"английский", "французский" }},
+               new Student {Name="Дмитрий", Age=29, Languages = new List<string> {"английский", "испанский" }},
+               new Student {Name="Василий", Age=24, Languages = new List<string> {"испанский", "немецкий" }}
+            };
+
+            // Составим запрос ()
+            var selectedStudents = students
+                .SelectMany(
+                   s => s.Languages, // коллекция, которую нужно преобразовать
+                   (s, l) => new { Student = s, Lang = l }) // функция преобразования, применяющаяся к каждому элементу коллекции                 
+               .Where(s => s.Lang == "английский" && s.Student.Age < 28) // дополнительные условия     
+               .Select(s => s.Student); // указатель на объект выборки
+
+            // Выведем результат
+            foreach (Student student in selectedStudents)
+                Console.WriteLine($"{student.Name} - {student.Age}");
+        }
+    }
+
+    internal class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public List<string> Languages { get; set; }
+    }
+}
---
+++ LINQ1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("LINQ1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LINQ1")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("45ad9d46-8d21-4d13-a0e7-d1ba73b3049c")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
