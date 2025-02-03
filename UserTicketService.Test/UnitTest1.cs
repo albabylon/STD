5f96d7eedab90980049fde8edc27b4c836b278d4
aveA <aveA@AVE>
2025-02-03 16:21:59 +0300

    Unit test

---
+++ UserTicketService.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace UserTicketService.Test
+{
+    public class Tests
+    {
+        [TestFixture]
+        public class Class1
+        {
+            [Test]
+            public void Test1()
+            {
+                Assert.True(50 == 100);
+            }
+        }
+    }
+}
