3ec32311be67ace67a4b5bed019ad0b9814204d2
babylone <barsukovvo@mail.ru>
2025-03-05 00:10:40 +0300

    testing

--- TestCommon/Program.cs
+++ TestCommon/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace TestCommon
 {
@@ -9,18 +10,68 @@ namespace TestCommon
 
         static void Main(string[] args)
         {
+            //try
+            //{
+            //    Method();
+            //}
+            //catch (Exception ex) 
+            //{ 
+
+            //}
+
+            //Console.WriteLine(string.Join(", ", Error));
+
+            //var ms = new MyStruct(5);
+            //using (ms)
+            //{
+            //    ms.Foo();
+            //}
+            //Console.WriteLine(ms.a);
+
+            //int iterator = 0;
+            //int[] array = { 1, 2, 3, 4, 5, 6 };
+            //var flter = array.Where(n => n > 3).Select(_ => iterator++);
+            //Console.WriteLine(flter.First());
+            //Console.WriteLine(iterator);
+
             try
             {
-                Method();
+                Action<string> action = new Action<string>(Console.WriteLine);
+                AddMethod(action);
+                action("Hello world");
             }
-            catch (Exception ex) 
-            { 
-
+            catch 
+            {
+                Console.WriteLine("Error");
             }
-
-            Console.WriteLine(string.Join(", ", Error));
 
             Console.ReadLine();
+        }
+
+        private static void AddMethod(Action<string> action)
+        {
+            action += Console.WriteLine;
+            action += Console.WriteLine;
+            action -= Console.WriteLine;
+        }
+
+        public struct MyStruct : IDisposable
+        {
+            public int a;
+            public MyStruct(int a)
+            {
+                this.a = a;
+            }
+
+            public void Foo()
+            {
+                a = 7;
+            }
+
+            public void Dispose()
+            {
+                a = 0;
+            }
         }
 
         private static void Method()
